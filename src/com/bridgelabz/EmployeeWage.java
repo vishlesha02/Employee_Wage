2b97833b4fe499324103ace12e02ae0b2807b57b
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-12 11:03:58 +0530

    Added : Employee Full Time Attendance

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,7 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Program");
+
+        int iSFullTime = 1;
+
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+
+        if (empCheck == iSFullTime)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+
+
     }
 }
