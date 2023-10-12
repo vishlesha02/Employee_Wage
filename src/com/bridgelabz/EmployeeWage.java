0df2493125beb23b6768294f316b0d13bd062f74
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-12 11:24:05 +0530

    Added : EmployeeWage Using Switch Case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,33 +4,32 @@ import java.util.Random;
 
 public class EmployeeWage {
     public static void main(String[] args) {
+        final int IsFullTime = 1;
+        final int IsPartTime = 2;
 
-        int iSFullTime = 1;
-        int isPartTime = 2;
         int empWagePerHour = 20;
         int fullDayHour = 8;
         int partTimeHour = 4;
-
         int salary;
 
         Random random = new Random();
         int empCheck = random.nextInt(3);
 
-        if (empCheck == iSFullTime)
-        {
-            System.out.println("Employee is Full Time Present");
-            salary = empWagePerHour*fullDayHour;
-            System.out.println("Employee Wage = " + salary);
-        }
-        else if(empCheck == isPartTime)
-        {
-            System.out.println("Employee is Part Time Present");
-            salary = empWagePerHour*partTimeHour;
-            System.out.println("Employee Wage = " + salary);
-        }
-        else
-        {
-            System.out.println("Employee is Absent");
+        switch (empCheck) {
+            case IsFullTime:
+                System.out.println("Employee is Full Time Present");
+                salary = empWagePerHour * fullDayHour;
+                System.out.println("Employee Wage = " + salary);
+                break;
+
+            case IsPartTime:
+                System.out.println("Employee is Part Time Present");
+                salary = empWagePerHour * partTimeHour;
+                System.out.println("Employee Wage = " + salary);
+                break;
+
+            default:
+                System.out.println("Employee is Absent");
         }
     }
 }
