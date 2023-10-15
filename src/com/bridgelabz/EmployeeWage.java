ec2133d9fd36498e4fd257a7aabfafe8eb64e930
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-15 23:27:38 +0530

    Added : Employee Wage for Multiple Companys

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,19 +5,16 @@ import java.util.Random;
 public class EmployeeWage {
     public static final int IsFullTime = 1;
     public static final int IsPartTime = 2;
-    static int EMP_WAGE_PER_HOUR = 20;
-    static int HOURS_PER_MONTH = 100;
-    static int DAYS_PER_MONTH = 20;
 
-    public void EmpWage() {
+    public int  EmpWage(String company,int empWagePerHour, int hoursPerMonth,int daysPerMonth) {
 
-        int empHours = 0;
-        int salaryPerMonth;
+        int empHours;
+        int salary = 0;
         int totalSalary = 0;
         int totalEmpHour = 0;
         int day = 0;
 
-        while (totalEmpHour < HOURS_PER_MONTH && day < DAYS_PER_MONTH) {
+        while (totalEmpHour < hoursPerMonth && day < daysPerMonth) {
             day++;
             Random random = new Random();
 
@@ -25,38 +22,35 @@ public class EmployeeWage {
 
             switch (empCheck) {
                 case IsFullTime:
-
-                    System.out.println("Employee is Full Time Present");
                     empHours = 8;
                     break;
 
                 case IsPartTime:
-
-                    System.out.println("Employee is Part Time Present");
                     empHours = 4;
                     break;
 
                 default:
-                    System.out.println("Employee is Absent");
                     empHours = 0;
 
             }
-            salaryPerMonth = empHours * EMP_WAGE_PER_HOUR;
-            System.out.println("Day " + day + " - salary : " + salaryPerMonth);
-            totalSalary += salaryPerMonth;
+            salary = empHours * empWagePerHour;
+            totalSalary += salary;
             totalEmpHour += empHours;
 
-        }
-        System.out.println("Total Working Days : " + day);
-        System.out.println("Total Employee Hrs : " + totalEmpHour);
-        System.out.println("Total Salary : " + totalSalary);
+            System.out.println("Day " + day + " - Employee Hrs : " + empHours + " - salary : " + salary );
 
+
+        }
+        System.out.println("- - - - - - - - - - - - - - - - - - - - ");
+        System.out.println("Total Employee Wage for "  + company + " is " + totalSalary);
+        System.out.println("- - - - - - - - - - - - - - - - - - - - ");
+        return totalSalary;
 
     }
     public static void main(String[] args) {
         EmployeeWage obj=new EmployeeWage();
-        obj.EmpWage();
+        obj.EmpWage("Jio",30,70,23);
+        obj.EmpWage("Reliance",20,50,21);
 
     }
-
 }
